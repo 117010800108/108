446d7a094e15e4e78d1a2b07eb07367797c56b28
117010800108 <43243903+117010800108@users.noreply.github.com>
2018-10-19 08:58:40 +0800

    Add files via upload

---
+++ ex2_1.py
@@ -0,0 +1,11 @@
+h=input("请输入温度符号")
+if h in['F','f']:
+    t=eval(input("请输入温度值："))
+    C=(t-32)/1.8
+    print("转换后的温度是{：.2f}C".format(C))
+elif h in['C','c']:
+    t=eval(input("请输入温度值:"))
+    F=1.8*t+32
+    print("转换后的温度是[：.2f]F".format(F))
+else:
+    print("输入格式错误")
